d6f75be5b0c5910a1e9f1d906762dc8c291c2ad7
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-11-28 10:22:26 -0600

    UserSignIn

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react'
+import {getAuth} from 'firebase/auth'
+
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  )
+
+const [user, setUser] = useState(null)
+
+const auth = getAuth()
+useEffect(() => {
+  setUser(auth.currentUser)
+}, [ ])
+//arrays should have a space indicating empty
+
+return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
 }
+
 export default Profile
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { doc, setDoc, serverTimestamp} from "firebase/firestore"
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -7,6 +10,7 @@ function SignUp() {
   const [showPassword, setShowPassword]= useState (false)
   const [formData, setFormData]= useState ({ name: '', email: '' , password: '',
  })
+//  above should have no space or placeholder will not work
   const {name, email, password} = formData
 
   const navigate = useNavigate()
@@ -17,6 +21,33 @@ function SignUp() {
     }))
   }
 
+  const onSubmit  = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth= getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+const formDataCopy = {...formData}
+delete formDataCopy.password
+formDataCopy.timestamp = serverTimestamp()
+
+await setDoc(doc (db, 'users', user.uid), formDataCopy)
+
+
+      navigate('/')
+
+    } catch (error) {
+     console.log(error)
+      
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -24,7 +55,7 @@ function SignUp() {
           <p className='pageHeader'>Please Sign Up.</p>
         </header>
 
-        <form>
+        <form onSubmit = {onSubmit}>
         <input
             type='text'
             className='nameInput'
